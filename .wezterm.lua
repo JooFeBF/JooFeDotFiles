9de4857db59ac1af281d6a6877fc62f9b729b633
Joseph Martinez <insertepala@gmail.com>
2024-05-13 19:25:22 -0500

    Latest changes

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,16 +10,17 @@ config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font("CaskaydiaCove Nerd Font")
 -- enable with your own background image
 --
--- config.background = {
--- 	{
--- 		source = {
--- 			File = "/path/to/image",
--- 		},
--- 		hsb = {
--- 			brightness = 0.1,
--- 			saturation = 0.8,
--- 		},
--- 	},
--- }
+config.background = {
+	{
+		source = {
+			File = "./Downloads/taigawp.jpg",
+		},
+		hsb = {
+			brightness = 0.1,
+		},
+	},
+}
+
+config.hide_tab_bar_if_only_one_tab = true
 -- and finally, return the configuration to wezterm
 return config
